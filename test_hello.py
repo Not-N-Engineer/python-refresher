c9e6e3006615ee3e8c7893e362371582a7763b1c
Yujie Jin <arceascompany@gmail.com>
2026-07-07 11:38:32 -0400

    Added one more test to each trig test.

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,6 @@
 import unittest
 import hello
+import numpy as np
 
 
 class TestHello(unittest.TestCase):
@@ -9,18 +10,22 @@ class TestHello(unittest.TestCase):
     def test_sin(self):
         self.assertEqual(hello.sin(0), 0)
         self.assertEqual(hello.sin(1), 0.8414709848078965)
+        self.assertEqual(hello.sin(np.pi/2), 1)
 
     def test_cos(self):
         self.assertEqual(hello.cos(0), 1)
         self.assertEqual(hello.cos(1), 0.5403023058681398)
+        self.assertEqual(hello.cos((np.pi)/2), np.cos((np.pi)/2))
 
     def test_tan(self):
         self.assertEqual(hello.tan(0), 0)
         self.assertEqual(hello.tan(1), 1.5574077246549023)
+        self.assertEqual(hello.tan(np.pi), np.tan(np.pi))
 
     def test_cot(self):
         self.assertEqual(hello.cot(0), float("inf"))
         self.assertEqual(hello.cot(1), 0.6420926159343306)
+        self.assertEqual(hello.cot((np.pi)/2), 1/np.tan((np.pi)/2))
 
 
 if __name__ == "__main__":

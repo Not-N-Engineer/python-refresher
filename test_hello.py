76448fe1ddac62221fe3ff2bb5c8eb1030126069
Yujie Jin <arceascompany@gmail.com>
2026-07-07 13:50:05 -0400

    Added tests for arithmatic.

--- test_hello.py
+++ test_hello.py
@@ -1,11 +1,33 @@
 import unittest
 import hello
 import numpy as np
+import pytest
 
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello(), "Hello, world!")
+    
+    def test_add(self):
+        assert hello.add(5, 3) == 8          # Test for positive numbers
+        assert hello.add(-2, 2) == 0         # Test for negative and positive number
+        assert hello.add(0, 0) == 0          # Test for zero addition
+
+    def test_sub(self):
+        assert hello.sub(5, 3) == 2     # Test for positive numbers
+        assert hello.sub(-2, -2) == 0    # Test for negative numbers
+        assert hello.sub(0, 5) == -5    # Test for zero minuend
+
+    def test_mul(self):
+        assert hello.mul(5, 3) == 15    # Test for positive numbers
+        assert hello.mul(-2, 2) == -4   # Test for negative and positive number
+        assert hello.mul(0, 100) == 0   # Test for multiplication by zero
+
+    def test_div(self):
+        assert hello.div(-4, 2) == -2     # Test for negative and positive number
+        assert hello.div(5, 2) == 2.5     # Test for division resulting in float
+        with pytest.raises(ValueError, match="Can't divide by zero!"):
+            hello.div(5, 0)               # Test division by zero
 
     def test_sin(self):
         self.assertEqual(hello.sin(0), 0)

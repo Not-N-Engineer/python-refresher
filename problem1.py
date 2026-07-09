cf54fac93f08abd786de7f841c881483c378b4e9
Yujie Jin <arceascompany@gmail.com>
2026-07-09 13:48:51 -0400

    Made problem1 and tests better in accodance with Dr. Saad's advice.

--- problem1.py
+++ problem1.py
@@ -1,20 +1,45 @@
 import numpy as np
 
-class BankAccount:
-    num_accounts = 0
+class Bank:
+    def __init__(self):
+        self.num_accounts = 0
 
+bank_of_auvc = Bank()
+
+
+class Account:
     def __init__(self, name, starting_balance):
-        BankAccount.num_accounts += 1
+        bank_of_auvc.num_accounts += 1
         self.name = name
         self.balance = starting_balance
-        self.account_num = BankAccount.num_accounts
+        self.account_num = bank_of_auvc.num_accounts
     
+    def check_for_overdraft(self, amount):
+        if not isinstance(amount, (int, float)):
+            raise TypeError
+        if self.balance + amount < 0:
+            print("Error: Withdrawing more than account balance. ")
+            return False # should not go through with transaction
+        else:
+            return True 
+
     def withdraw(self, amount):
-        self.balance -= amount
+        if self.check_for_overdraft(-amount):
+            self.balance -= amount
+        else:
+            return "Error: Withdrawing more than account balance. "
     
     def deposit(self, amount):
-        self.balance += amount
-    
-    def printBalance(self):
-        print(self.balance)
+        if self.check_for_overdraft(amount):
+            self.balance += amount
+        else:
+            return "Error: Withdrawing more than account balance. "
+
+    def getBalance(self):
         return self.balance
+    
+    def getAccountInfo(self):
+        return f"{self.name}'s account (Number: {self.account_num}) has a balance of ${self.balance}"
+
+    def __str__(self):
+        return f"{self.name}'s account (Number: {self.account_num}) has a balance of ${self.balance}"
--- test_problem1.py
+++ test_problem1.py
@@ -1,49 +1,49 @@
 import unittest
 import problem1
 import numpy as np
-import pytest
 
 
 class TestProblem1(unittest.TestCase):
     def test_init(self):
-        new_account = problem1.BankAccount("John", 0)
-        assert new_account == new_account
+        new_account = problem1.Account("John", 0)
+        self.assertEqual(new_account.getAccountInfo(), f"John's account (Number: {problem1.bank_of_auvc.num_accounts}) has a balance of $0")
 
     def test_withdraw(self):
-        new_account = problem1.BankAccount("John", 10000)
+        new_account = problem1.Account("John", 10000)
         new_account.withdraw(10000)
-        assert new_account.balance == 0
+        self.assertEqual(new_account.balance, 0)
 
-        new_account = problem1.BankAccount("Emily", -200)
-        new_account.withdraw(-10000)
-        assert new_account.balance == 9800
+        new_account = problem1.Account("Emily", -200)
+        self.assertNotEqual(new_account.withdraw(-10000), "Error: Withdrawing more than account balance. ")
+        self.assertEqual(new_account.balance, 9800)
+        self.assertNotEqual(new_account.balance, -10200)
 
-        new_account = problem1.BankAccount("Serena", 40)
-        new_account.withdraw(10000)
-        assert new_account.balance == -9960
+        new_account = problem1.Account("Serena", 40)
+        self.assertEqual(new_account.withdraw(10000), "Error: Withdrawing more than account balance. ")
+        self.assertNotEqual(new_account.balance, -9960)
     
     def test_deposit(self):
-        new_account = problem1.BankAccount("John", 10000)
+        new_account = problem1.Account("John", 10000)
         new_account.deposit(10000)
-        assert new_account.balance == 20000
+        self.assertEqual(new_account.balance, 20000)
 
-        new_account = problem1.BankAccount("Emily", -200)
-        new_account.deposit(-10000)
-        assert new_account.balance == -10200
+        new_account = problem1.Account("Emily", -200)
+        self.assertEqual(new_account.deposit(-10000), "Error: Withdrawing more than account balance. ")
+        self.assertNotEqual(new_account.balance, -10200)
 
-        new_account = problem1.BankAccount("Serena", 40)
+        new_account = problem1.Account("Serena", 40)
         new_account.deposit(5)
-        assert new_account.balance == 45
+        self.assertEqual(new_account.balance, 45)
     
-    def test_printBalance(self):
-        new_account = problem1.BankAccount("John", 10000)
-        assert new_account.printBalance() == 10000
+    def test_getBalance(self):
+        new_account = problem1.Account("John", 10000)
+        self.assertEqual(new_account.getBalance(), 10000)
 
-        new_account = problem1.BankAccount("Emily", -200)
-        assert new_account.printBalance() == -200
+        new_account = problem1.Account("Emily", -200)
+        self.assertEqual(new_account.getBalance(), -200)
 
-        new_account = problem1.BankAccount("Serena", 40)
-        assert new_account.printBalance() == 40
+        new_account = problem1.Account("Serena", 40)
+        self.assertEqual(new_account.getBalance(), 40)
     
 if __name__ == "__main__":
     unittest.main()
